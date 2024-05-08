1cdd0c65bf7f5eb299763187af1c73d1ae4aa86a
Visushanmugam <visu7503@gmail.com>
2024-05-08 09:25:36 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+"""This file using libraries"""
+from setuptools import setup, find_packages
+
+setup(
+    name="Mathematical_Symbol_Model",
+    version="0.0.1",
+    author="Visu",
+    author_email="visu@visha.xyz",
+    install_requires=['numpy'],
+    packages=find_packages()
+)
